ba560f851f2b2820d3b7835c7fd74a845715e617
Panakotic <achuzhinova21@gmail.com>
2023-10-06 20:51:02 +0700

    FirstTask

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,30 @@
+import java.util.Arrays;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
+    public static void find_more(int[] mas) {
+            int last = mas[mas.length-1]*mas[mas.length-1];
+            int counter = 0;
+            for (int i = 0; i < mas.length; i++){
+                if (mas[i] > last) {
+                    counter++;
+                }
+            }
+            int[] result = new int[counter];
+            int j = 0;
+            for (int i = 0; i < mas.length; i++){
+                if (mas[i] > last){
+                    result[j] = mas[i];
+                    j++;
+                }
+            }
+            System.out.println(Arrays.toString(result));
+
+    }
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int[] mas = {2,3,4,5,6,1};
+        Main.find_more(mas);
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
     }
 }
